4ba849be4208925a060b518937dd79423f47cfa0
Oleksandra Matiash-Rybachuk <69486420+piiikaaachuuu@users.noreply.github.com>
2023-03-07 23:13:20 +0200

    Add files via upload

--- task-5.js
+++ task-5.js
@@ -1,8 +1,9 @@
 const array1 = [5, 10, 15, 20];
 const array2 = [10, 20, 30];
-const result = [];
+const array3 = array1.concat(array2);
+let result = Number([]);
 
-for (let i = 0; i < array1.length; i += 1) {
-  result.push(array1[i] + array2[i]);
+for (let i = 0; i < array3.length; i += 1) {
+  result += array3[i];
 }
 console.log(result);
